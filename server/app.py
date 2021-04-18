6ab79bf7ea401d63c6aa2d67c031f091d605ecaa
Max Larkin <maxlarkin@protonmail.com>
2021-04-18 17:29:20 +0100

    Server endpoints for basic CRD on models

--- server/app.py
+++ server/app.py
@@ -1,8 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from flask import jsonify
-from database import add_gesture
-
+from database import *
 app = Flask(__name__)
 CORS(app)
 
@@ -12,4 +11,26 @@ def gesture():
     checked = json_data["checked"]
     data = json_data["data"]
     add_gesture(checked, data)
+    return jsonify(success=True)
+
+@app.route('/create_model', methods=['POST'])
+def create_model():
+    json_data = request.get_json()
+    name = json_data["name"]
+    add_model(name)
+    return jsonify(success=True)
+
+@app.route('/get_models', methods=['GET'])
+def get_models():
+    return {"models": get_all_models()}
+
+@app.route('/get_model', methods=['POST'])
+def get_model():
+    model_id = request.get_json()["id"]
+    return get_gesture_model(model_id)
+
+@app.route('/delete_model', methods=['POST'])
+def delete_model():
+    model_id = request.get_json()["id"]
+    delete_gesture_model(model_id)
     return jsonify(success=True)
