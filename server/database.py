71d8b94b4972125e3643862d18c8d4c79ba80ef5
Max Larkin <maxlarkin@protonmail.com>
2021-04-18 17:27:37 +0100

    Adding models w/ relevant queries to database

--- server/database.py
+++ server/database.py
@@ -2,9 +2,12 @@ from pony.orm import *
 
 db = Database()
 
-db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+class Model(db.Entity):
+    name = Required(str)
+    gestures = Set('Gesture')
 
 class Gesture(db.Entity):
+    model = Required(Model)
     data = Set('DataTick')
     classification = Required(bool)
 
@@ -14,7 +17,9 @@ class DataTick(db.Entity):
     accelerometerZ = Required(float)
     gesture = Required(Gesture)
 
-db.generate_mapping(create_tables=True)
+def setup_database():
+    db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+    db.generate_mapping(create_tables=True)
 
 @db_session
 def add_gesture(checked, data):
@@ -30,3 +35,22 @@ def get_all_gestures():
                     for d in g.data)[:]
     print(f"len res: {len(result)}")
     return result
+
+@db_session
+def add_model(name):
+    model = Model(name=name)
+    commit()
+
+@db_session
+def get_all_models():
+    result = select((m.id, m.name) for m in Model)[:]
+    return [{"id": i, "name": v} for i, v in result]
+
+@db_session
+def delete_gesture_model(model_id):
+    Model[model_id].delete()
+    commit()
+
+@db_session
+def get_gesture_model(model_id):
+    return Model[model_id].to_dict(with_collections=True)
