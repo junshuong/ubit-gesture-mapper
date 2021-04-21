414cb7c141bfa53117311bf8ea07d5d0cca9c515
Max Larkin <maxlarkin@protonmail.com>
2021-04-21 20:03:45 +0100

    Enable real time guesture recognition

--- client/src/features/models/Model.tsx
+++ client/src/features/models/Model.tsx
@@ -1,12 +1,13 @@
 import { Button, Checkbox, IconButton, makeStyles, Paper, Tooltip, Typography } from '@material-ui/core';
 import InfoIcon from '@material-ui/icons/Info';
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { store } from '../../app/store';
 import cfg from '../../config.json';
-import { selectActiveModel, setActiveModel } from './activeModelSlice';
+import { activate, selectActiveModel, setActiveModel } from './activeModelSlice';
+import * as tf from '@tensorflow/tfjs';
 
 const useStyles = makeStyles({
     root: {
@@ -32,6 +33,8 @@ export function Model(props: { match: { params: { id: any } }, history: string[]
     useEffect(() => {
         fetchModel(id);
     }, [id])
+
+    const [weightMap, setWeightMap] = useState({});
 
     return (
         <div>
@@ -63,12 +66,80 @@ export function Model(props: { match: { params: { id: any } }, history: string[]
             <Paper className={classes.root} variant="outlined">
                 <Typography variant="h4">Loading the model</Typography>
                 <Typography variant="body1">Enable the model using the button below and the status of the ouput will be indicated on the checkboxes below.</Typography>
-                <Button color="primary" variant="contained"><Typography>Load Model</Typography></Button>
-                <Typography>Is Active <Checkbox disabled /></Typography>
-                <Typography>Triggered <Checkbox disabled /></Typography>
+                <Button color="primary" variant="contained" onClick={() => {
+                    loadModel(activeModel.id).then((res) => {
+                        setWeightMap(res);
+                    })
+                }
+                }><Typography>Load Model</Typography></Button>
+                <Typography>Is Active <Checkbox disabled checked={activeModel.isActive} /></Typography>
+                <ModelOutput weightMap={weightMap}/>
             </Paper>
         </div>
     );
+}
+
+async function loadModel(id: number) {
+    console.info("Requesting Tensorflow model...");
+    const weightsManifest: any = await fetchWeightsManifest(id);
+    const weightMap = await tf.io.loadWeights(weightsManifest, `${cfg.server_url}/get_model_part/${id}`);
+    store.dispatch(activate());
+    return await weightMap;
+}
+
+function ModelOutput(props: { weightMap: {} }) {
+    const activeModel = useSelector(selectActiveModel);
+
+    const [on, setOn] = useState(0);
+    const [off, setOff] = useState(0);
+
+    let flattened: number[] = []
+    
+    activeModel.history.forEach(el => {
+        flattened.push(el.x)
+        flattened.push(el.y)
+        flattened.push(el.z)
+    });
+
+    if (flattened.length === activeModel.tickCount * 3 && activeModel.tickCount !== 0) {
+        forwardPass(props.weightMap, flattened).then((output: any) => {
+            setOn(output[0]);
+            setOff(output[1]);
+        })
+    }
+    return(
+        <div>
+            <Typography>Triggered <Checkbox disabled checked={on > off} /></Typography>
+            <div>On</div>
+            <div>{on}</div>
+            <div>Off</div>
+            <div>{off}</div>
+        </div>
+    );
+}
+
+async function forwardPass(weightMapPromise: tf.NamedTensorMap, data: number[]) {
+
+    const weightMap = await weightMapPromise;
+
+    if (Object.keys(weightMap).length < 1) return [0, 0];
+
+    const input = tf.tensor2d(data, [1, 120])
+    
+    const fc1_bias = weightMap['StatefulPartitionedCall/sequential/dense/BiasAdd/ReadVariableOp'];
+    const fc1_weight = weightMap['StatefulPartitionedCall/sequential/dense/MatMul/ReadVariableOp'];
+    const fc2_bias = weightMap['StatefulPartitionedCall/sequential/dense_1/BiasAdd/ReadVariableOp']
+    const fc2_weight = weightMap['StatefulPartitionedCall/sequential/dense_1/MatMul/ReadVariableOp']
+    // const fc3 = weightMap['StatefulPartitionedCall/sequential/flatten/Const']
+    const itf1 = tf.matMul(input, fc1_weight).add(fc1_bias);
+    const f1tf2 = tf.matMul(itf1, fc2_weight).add(fc2_bias);
+    return await f1tf2.flatten().array();
+}
+
+function fetchWeightsManifest(id: number) {
+    return axios.post(`${cfg.server_url}/get_trained_model`, { id: id }).then((res) => {
+        return (res.data['weightsManifest']);
+    }).catch((err) => { return err; });
 }
 
 export function fetchModel(id: number) {
