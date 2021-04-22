8f7e5ac6d44a6d3677e3bf96f3a801ff5f6844d6
Dylan Gore <hello@dylangore.ie>
2021-04-22 09:45:25 +0100

    Remove Audio component from router and navigation
    The audio component is now part of the model component and no longer needs a router ink

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,6 @@ import { Recorder } from './features/recorder/Recorder';
 import { Models } from './features/models/Models';
 import { connectMicrobitDevice, disconnectMicrobit } from './uBit/uBit';
 import { Model } from './features/models/Model';
-import { Audio } from './features/audio/Audio'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -37,11 +36,7 @@ function App() {
           <Route exact path="/models">
             <Models />
           </Route>
-          <Route exact path="/model/:id" component={Model}/>
-          {/* Temporary link fro development purposes */}
-          <Route exact path="/audio">
-            <Audio />
-          </Route>
+          <Route exact path="/model/:id" component={Model} />
           <Route path="/">
             <DataHeader />
             <MicrobitData />
@@ -69,12 +64,6 @@ function RecordHeader() {
         <Link to="/models" className={classes.link}>
           <Button color="inherit">
             Models
-          </Button>
-        </Link>
-        {/* Temporary link fro development purposes */}
-        <Link to="/audio" className={classes.link}>
-          <Button color="inherit">
-            Audio
           </Button>
         </Link>
       </Toolbar>
