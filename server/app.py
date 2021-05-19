ce68367b18722ea45e966adb8a2e83eca3f70d96
Dylan Gore <hello@dylangore.ie>
2021-05-19 11:08:16 +0100

    Add support for writing to InfluxDB

--- server/app.py
+++ server/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, json, request
 from flask.helpers import send_file
 from flask_cors import CORS
-from flask import jsonify
+from flask import jsonify, Response
 from convert import train_new_model
 from database import *
-
+from influx import write_to_influx
+import toml
+import logging
 
 app = Flask(__name__)
 CORS(app)
 
 setup_database()
+
 
 @app.route('/gesture', methods=['POST'])
 def gesture():
@@ -21,6 +24,7 @@ def gesture():
     train_new_model(model_id)
     return jsonify(success=True)
 
+
 @app.route('/create_model', methods=['POST'])
 def create_model():
     json_data = request.get_json()
@@ -30,14 +34,17 @@ def create_model():
     add_model(name, description, tickCount)
     return jsonify(success=True)
 
+
 @app.route('/get_models', methods=['GET'])
 def get_models():
     return {"models": get_all_models()}
+
 
 @app.route('/get_model', methods=['POST'])
 def get_model():
     model_id = request.get_json()["id"]
     return get_gesture_model(model_id)
+
 
 @app.route('/delete_model', methods=['POST'])
 def delete_model():
@@ -45,11 +52,13 @@ def delete_model():
     delete_gesture_model(model_id)
     return jsonify(success=True)
 
+
 @app.route('/train_model', methods=['POST'])
 def train_model():
     model_id = request.get_json()["id"]
     train_new_model(model_id)
     return jsonify(success=True)
+
 
 @app.route('/get_trained_model', methods=['POST'])
 def get_trained_model():
@@ -57,6 +66,25 @@ def get_trained_model():
     f = open(f"./learners/model-{model_id}/web_model/model.json")
     return json.load(f)
 
+
 @app.route('/get_model_part/<id>/<file>', methods=['GET'])
 def get_model_part(id, file):
     return send_file(f"./learners/model-{id}/web_model/{file}")
+
+
+@app.route('/save_to_db', methods=['POST'])
+def save_to_db():
+    """POST route to handle data that should be written to InfluxDB"""
+    try:
+        # Attempt to load the config file before calling the function to ensure it's present
+        CONFIG = toml.load("config.toml")["influxdb"]
+        # Get the required data from the request
+        gesture_name = request.get_json()["gesture_name"]
+        fields = request.get_json()["fields"]
+        print(request.get_json())
+        # Call the function to write to the database
+        return write_to_influx(gesture_name, fields)
+    except (FileNotFoundError, KeyError) as err:
+        # Return a 500 error if the config is missing or data is malformed
+        logging.error(err)
+        return Response(status=500, mimetype='application/json')
