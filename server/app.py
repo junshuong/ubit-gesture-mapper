489deb518a325f4309a8c6e9cf5597c0f1d57bc0
Max Larkin <maxlarkin@protonmail.com>
2021-05-20 17:31:09 +0100

    API endpoints and refactoring

--- server/app.py
+++ server/app.py
@@ -11,34 +11,53 @@ CORS(app)
 
 setup_database()
 
-@app.route('/gesture', methods=['POST'])
-def gesture():
-    json_data = request.get_json()
-    checked = json_data["checked"]
-    model_id = json_data["model_id"]
-    accel_data = json_data["acclerometerData"]
-    magnet_data = json_data["magnetometerData"]
-    add_gesture(checked, accel_data, magnet_data, model_id)
-    train_new_model(model_id)
-    return jsonify(success=True)
-
 @app.route('/create_model', methods=['POST'])
 def create_model():
     json_data = request.get_json()
     name = json_data["name"]
-    tickCount = json_data["tickCount"]
     description = json_data["description"]
-    add_model(name, description, tickCount)
+    add_model(name, description)
     return jsonify(success=True)
+
+
+@app.route('/create_gesture', methods=['POST'])
+def add_gesture():
+    json_data = request.get_json()
+    name = json_data["name"]
+    model_id = json_data["id"]
+    classification = json_data["classification"]
+    create_gesture(name, model_id, classification)
+    return jsonify(success=True)
+
+
+@app.route('/add_capture', methods=['POST'])
+def add_capture():
+    json_data = request.get_json()
+    classification = json_data["classification"]
+    gesture_id = json_data["gesture_id"]
+    accel_data = json_data["acclerometerData"]
+    magnet_data = json_data["magnetometerData"]
+    create_capture(classification, accel_data, magnet_data, gesture_id)
+    return jsonify(success=True)
+
+@app.route('/get_gesture/<gesture_id>', methods=['GET'])
+def get_gesture(gesture_id):
+    return get_gesture_from_db(gesture_id)
+
 
 @app.route('/get_models', methods=['GET'])
 def get_models():
     return {"models": get_all_models()}
 
-@app.route('/get_model', methods=['POST'])
-def get_model():
-    model_id = request.get_json()["id"]
-    return get_gesture_model(model_id)
+@app.route('/get_model/<model_id>', methods=['GET'])
+def get_model(model_id):
+    return get_model_from_db(model_id)
+
+@app.route('/remove_gesture', methods=['POST'])
+def remove_gesture():
+    g_id = request.get_json()["gesture_id"]
+    delete_gesture(g_id)
+    return jsonify(success=True)
 
 @app.route('/delete_model', methods=['POST'])
 def delete_model():
@@ -61,3 +80,4 @@ def get_trained_model():
 @app.route('/get_model_part/<id>/<file>', methods=['GET'])
 def get_model_part(id, file):
     return send_file(f"./learners/model-{id}/web_model/{file}")
+
--- server/convert.py
+++ server/convert.py
@@ -8,24 +8,7 @@ from platform import system
 
 def import_data(model_id):
     raw_data = get_all_gestures(model_id)
-    records = {}
-    for gesture in raw_data:
-        id = gesture[0]
-        target = gesture[1]
-        ax = gesture[2]
-        ay = gesture[3]
-        az = gesture[4]
-        mx = gesture[5]
-        my = gesture[6]
-        mz = gesture[7]
-        if id not in records:
-            records[id] = {}
-        records[id]["target"] = target
-        if "data" not in records[id]:
-            records[id]["data"] = []
-        records[id]["data"].append([ax, ay, az, mx, my, mz])
-    return list(records.values())
-
+    return [(g[0:-1], g[-1]) for g in raw_data]
 
 def random_training_sample(data):
     return data[random.randint(0, len(data)-1)]
@@ -71,19 +54,18 @@ def train_new_model(model_id):
     # 1 Gesture, 180 data points (30 x, y, z accelerometer values and 30 x, y, z magnetometer values)
     input_shape = (1, 180)
     classifier_count = 2  # Number of unique possible outputs
-
+    print("Creating model")
     # Creating the classifier
     model = create_model(input_shape, classifier_count)
     # Importing the data from the database
-    training_data = import_data(model_id)
-    input_tensors, target_tensors = all_training_samples(
-        training_data)  # Splitting data into x and y
+    training_data = list(zip(*import_data(model_id)))
+    input_tensors, target_tensors = training_data[0], training_data[1]
     print(f"Length inputs: {len(input_tensors[0])}")
     # Shaping x and y (because tensorflow)
-    train_x = tf.concat(input_tensors, 0)
-    train_y = tf.concat(target_tensors, 0)
+    # train_x = tf.concat(input_tensors, 0)
+    # train_y = tf.concat(target_tensors, 0)
 
-    model.fit(train_x, train_y, epochs=20)  # Fitting the data
+    model.fit(input_tensors, target_tensors, epochs=20)  # Fitting the data
     model.save_weights(f"./checkpoints/model-{model_id}")
     # Saving the raw model
     tf.saved_model.save(model, f"./learners/model-{model_id}")
